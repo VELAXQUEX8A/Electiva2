805e12bf989744f789fa2d37e24dcb456309934c
Juan Velasquez <juanjvelasquez8a@gmail.com>
2025-08-28 18:55:32 -0500

    Primer comit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Pequeña animación de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
